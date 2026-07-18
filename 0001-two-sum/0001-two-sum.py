ca320f31146aea9c1f7dbdd10a1968aa07717f82
Yaswanth Kumar <122212736+yaswanthkumar-yarragudi@users.noreply.github.com>
2026-07-18 09:44:08 +0530

    Time: 579 ms (33.14%), Space: 19.7 MB (94.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,18 +5,20 @@ class Solution:
 
         for i in nums:
             need = target - i
-            if need in nums:
+            ind = (nums.index(i))
+            if need in nums[ind+1:]:
                 first = nums.index(i)
-                second = nums.index(need)
+                second = nums.index(need,ind+1)
+                return [first,second]
 
-                if first != second:
-                    return [first,second]
-                try:
-                    first = nums.index(i)
-                    second = nums.index(need,first+1)            
-                    return [first,second]
-                except ValueError:
-                    pass
+                # if first != second:
+                #     return [first,second]
+                # try:
+                #     first = nums.index(i)
+                #     second = nums.index(need,first+1)            
+                #     return [first,second]
+                # except ValueError:
+                #     pass
         return []
 
                     
